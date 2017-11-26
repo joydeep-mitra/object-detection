2f07765bce9c8b62cbd97b07290ad53ebf48c730
Joydeep Mitra <joydeep@imag.ai>
2017-11-26 17:42:06 -0600

    Removed extraneous commands

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 docker pull deeone/tensorflow-object-detection:1.2.1
-git clone https://github.com/joydeep-mitra/object-detection.git
-cd object-detection
 docker build . -t object-detection:1.0.0
