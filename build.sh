f1ca0011d4e6e36257c58651d7fc9994e8cc4865
Joydeep Mitra <joydeep@imag.ai>
2017-11-26 17:34:38 -0600

    Made the file executable

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 docker pull deeone/tensorflow-object-detection:1.2.1
 git clone https://github.com/joydeep-mitra/object-detection.git
 cd object-detection
-docker build . -t object-recognition:1.0.0
+docker build . -t object-detection:1.0.0
