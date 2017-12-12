572b15f3171a37cc62c56a2e61584843d82688fe
Joydeep Mitra <joydeep@imag.ai>
2017-12-12 00:49:01 -0600

    Minor typos

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /notebooks
-cp object_detection/imag.ai/create_imagai_tf_record.py models/research/object_detection/dataset_tools/.
-cp -rf object_detection/imag.ai/tools_run models/research/object_detection/imagai_run
-mv object_detection/imag.ai/tools_run models/research/object_detection/imagai_run/tools object_detection/imag.ai/tools_run models/research/object_detection/imagai_run/data
+cp object-detection/imag.ai/create_imagai_tf_record.py models/research/object_detection/dataset_tools/.
+cp -rf object-detection/imag.ai/tools_run models/research/object_detection/imagai_run
+mv models/research/object_detection/imagai_run/tools models/research/object_detection/imagai_run/data
 mkdir models/research/object_detection/imagai_run/models
