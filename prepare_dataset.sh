c9c9222c1eaf29aa3fdb64860f0dd19b43befab3
Joydeep Mitra <joydeep@imag.ai>
2017-12-14 01:05:06 -0600

    Removed typo

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -5,4 +5,5 @@ export DATA_DIR=./object_detection/imagai_run/data
 python object_detection/dataset_tools/create_imagai_tf_record.py \
     --label_map_path=$DATA_DIR/tools_label_map.pbtxt \
     --data_dir=$DATA_DIR \
-    --output_dir=`$DATA_DIR
+    --output_dir=$DATA_DIR
+
