01d3ce60853b5664871e8283d71476baed6aa358
Joydeep Mitra <joydeep@imag.ai>
2017-12-17 16:37:25 -0600

    Fixed typo

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -3,7 +3,7 @@
 cd /notebooks/models/research
 export DATA_DIR=./object_detection/imagai_run/data
 python object_detection/dataset_tools/create_imagai_tf_record.py \
-    --label_map_path=$DATA_DIR/tools_label_map.pbtxt \
+    --label_map_path=$DATA_DIR/tool_label_map.pbtxt \
     --data_dir=$DATA_DIR \
     --output_dir=$DATA_DIR
 
