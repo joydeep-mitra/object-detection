5b23ee2e3d7a6f1edbafc3538e1c71d45638fd17
Joydeep Mitra <joydeep@imag.ai>
2017-12-19 00:24:30 -0600

    Downloading model

--- setup.sh
+++ setup.sh
@@ -5,5 +5,13 @@ cp object-detection/imag.ai/create_imagai_tf_record.py models/research/object_de
 cp -rf object-detection/imag.ai/tools_run models/research/object_detection/imagai_run
 mv models/research/object_detection/imagai_run/tools models/research/object_detection/imagai_run/data
 mkdir models/research/object_detection/imagai_run/models
+mkdir models/research/object_detection/imagai_run/models/train
+mkdir models/research/object_detection/imagai_run/models/eval
+mv models/research/object_detection/imagai_run/faster_rcnn_resnet101_tools.config models/research/object_detection/imagai_run/models/.
+
 pip install lxml
 apt-get install python-tk
+apt-get install wget
+
+wget -O models/research/object_detection/imagai_run/models/faster_rcnn.tar.gz http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_2017_11_08.tar.gz
+tar -xzvf models/research/object_detection/imagai_run/models/faster_rcnn.tar.gz
